75efd4c72d6a0f5470d0dca3bbf33115721cdf41
mircojs <minimalism_zddq@163.com>
2024-12-20 17:52:40 +0800

    feat: 初始化 mini-ui-react 组件

---
+++ packages/mini-ui-react/ui/BBBtn/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface BBBtnProps {}
+
+export default function BBBtn(props: BBBtnProps) {
+  const {} = props;
+  return <button>btn</button>;
+}
---
+++ packages/mini-ui-react/ui/index.ts
@@ -0,0 +1,1 @@
+export { default as BBBtn } from "./BBBtn";
