1af0053a6093595a26205062bf6a1b24e73e509c
mircojs <minimalism_zddq@163.com>
2024-12-20 19:22:17 +0800

    初始化项目

--- packages/mini-ui-react/ui/BBBtn/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-interface BBBtnProps {}
-
-export default function BBBtn(props: BBBtnProps) {
-  const {} = props;
-  return <button>btn</button>;
-}
--- packages/mini-ui-react/ui/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as BBBtn } from "./BBBtn";
