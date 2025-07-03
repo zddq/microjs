b0a9f7ef51f7e4525925e48a8a53db238f4a1be0
曾代段秋 <zengdaiduanqiu1@yupaowang.com>
2025-07-03 16:34:19 +0800

    build: 更新项目结构和配置
    - 移除 cookie 包的源码和相关文件
    - 更新 README.md 和 package.json 内容
    - 删除多余的依赖项和配置文件

--- packages/cookie/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-///<reference types="./global.d.ts" />
-
-import type { GetServerSidePropsContext } from "next";
-
-/**
- * 浏览器/SSR-nextjs cookie 操作
- */
-declare const MiniCookie: IStatic<IMiniCookieData, GetServerSidePropsContext>;
-export default MiniCookie
